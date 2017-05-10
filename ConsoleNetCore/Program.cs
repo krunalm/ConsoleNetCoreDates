7d1a99c9dfa5aadf240083c2ce4f51a717c25094
Krunal <krunalm@emgage.com>
2017-05-10 19:45:11 +0530

    Sample dates app

---
+++ ConsoleNetCore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var dt = DateTime.Now.Date;
+            var nextDt = DateTime.Now;
+
+            for (int i = 1; i <= 14; i++)
+            {
+                nextDt = dt.AddDays(4 * i);
+                Console.WriteLine(nextDt);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine(Environment.NewLine + $"Elapsed Time(ms):{stopwatch.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+    }
+}
